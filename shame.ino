79bf4e248a6bd392757cfad1546d5a3197baaf6c
Brandon Kvarda <bjkvarda@gmail.com>
2015-08-05 20:35:36 -0700

    The actual code...

--- shame.ino
+++ shame.ino
@@ -1,44 +1,57 @@
 
 int buttonCurrent = 0;
+int pwrCurrent = 1;
 
 int button = D1;
-int pwr = D0;
-char buttonString[40];
+int pwr = A0;
 
+
+char buttonString[40];
+char pwrString[40];
 
 void setup() {
 
-pinMode(button, OUTPUT);
-pinMode(pwr, INPUT);
-
+pinMode(button, INPUT_PULLDOWN);
+pinMode(pwr, OUTPUT);
 digitalWrite(pwr, HIGH);
- 
+
+
 }
 
 
 void loop() {
  
+
  //get the state of the button
 
  buttonCurrent = digitalRead(button);
+ 
+ //turn it into a string (used for debugging)
  sprintf(buttonString, "%d", buttonCurrent);
  
- //if the button is pressed
+ pwrCurrent = digitalRead(pwr);
+ 
+ //turn it into a string (used for debugging)
+ sprintf(pwrString, "%d", pwrCurrent);
+ 
+ 
+ 
+ //if the button is not pressed
 
  
  if(buttonCurrent == HIGH){
      
-     
      //SHAME! *DING DING DING* 
      Spark.publish("shamedingdingding","such_shame");
-     delay(1000);
+     delay(200);
+    
      
      
  }
  else{
      
+     
      //do nothing
-    
      
  }
  
